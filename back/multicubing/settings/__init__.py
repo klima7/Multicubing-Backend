9ef95d06fdbc5cf33c4da9ef119e573fe8805553
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-01-31 22:32:25 +0100

    .dotenv support added

--- back/multicubing/settings/__init__.py
+++ back/multicubing/settings/__init__.py
@@ -1,8 +1,11 @@
 import os
 
+from dotenv import load_dotenv
+
 DEFAULT_MULTICUBING_ENVIRONMENT = 'development'
 
 
 def setup_settings():
+    load_dotenv()
     os.environ.setdefault('MULTICUBING_ENVIRONMENT', DEFAULT_MULTICUBING_ENVIRONMENT)
     os.environ['DJANGO_SETTINGS_MODULE'] = 'multicubing.settings.' + os.environ.get('MULTICUBING_ENVIRONMENT')
