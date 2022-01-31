74428731b2616176c8c960688dd1765ccb1e5e47
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-01-31 15:21:12 +0100

    Index page created

--- back/multicubing/urls.py
+++ back/multicubing/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from index.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', index),
+    path('admin/', admin.site.urls, name='admin'),
     path('api/', include('core.urls'))
 ]
