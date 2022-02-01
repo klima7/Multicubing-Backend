dc39b358cb63177278b323c5f7fa8b148c802a94
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-02-01 23:44:44 +0100

    Register endpoint adjusted in swagger

--- back/multicubing/apps/account/views.py
+++ back/multicubing/apps/account/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from drf_yasg.utils import swagger_auto_schema
 
 from .models import Account
 from .serializers import AccountSerializer, RegistrationSerializer
@@ -10,6 +11,7 @@ class AccountViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = AccountSerializer
     queryset = Account.objects.all()
 
+    @swagger_auto_schema(request_body=RegistrationSerializer, responses={201: None})
     @action(detail=False, methods=['POST'])
     def register(self, request):
         serializer = RegistrationSerializer(data=request.data)
