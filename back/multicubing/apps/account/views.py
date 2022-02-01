6af6207ed299dbb59cd22f501518fc6389fca8e2
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-02-01 14:43:53 +0100

    Registration endpoint created

--- back/multicubing/apps/account/views.py
+++ back/multicubing/apps/account/views.py
@@ -1,8 +1,24 @@
 from rest_framework import viewsets
-from .serializers import AccountSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
 from .models import Account
+from .serializers import AccountSerializer, RegistrationSerializer
 
 
 class AccountViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = AccountSerializer
     queryset = Account.objects.all()
+
+    @action(detail=False, methods=['POST'])
+    def register(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            account = serializer.save()
+            data['response'] = 'successfully registered a new user.'
+            data['email'] = account.email
+            data['username'] = account.username
+        else:
+            data = serializer.errors
+        return Response(data)
