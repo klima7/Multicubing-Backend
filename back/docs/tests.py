98129a438f8ebd9318beea696c40657549d9621a
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-02-02 12:07:32 +0100

    Unit tests fail test

--- back/docs/tests.py
+++ back/docs/tests.py
@@ -3,9 +3,9 @@ from django.test import TestCase
 
 class AnimalTestCase(TestCase):
 
-    # def test_fail(self):
-    #     """Animals that can speak are correctly identified"""
-    #     self.assertEqual(2, 1)
+    def test_fail(self):
+        """Animals that can speak are correctly identified"""
+        self.assertEqual(2, 1)
 
     def test_success(self):
         """Animals that can speak are correctly identified"""
