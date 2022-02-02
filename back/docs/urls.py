05fd0d089b77fa0ae643584901f4c60764f94edd
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-02-02 12:56:01 +0100

    Swagger and Redoc unit tests added

--- back/docs/urls.py
+++ back/docs/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -17,8 +17,7 @@ schema_view = get_schema_view(
 app_name = 'docs'
 
 urlpatterns = [
-    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='swagger-json'),
-    path('swagger.yml', schema_view.without_ui(cache_timeout=0), name='swagger-yml'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='swagger-without-ui'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-ui'),
 ]
