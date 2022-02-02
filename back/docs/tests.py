0436afbb7f4c16d3a8b577eda48a230b6861077c
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-02-02 11:59:29 +0100

    Unit tests workflow added

--- back/docs/tests.py
+++ back/docs/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class AnimalTestCase(TestCase):
+
+    # def test_fail(self):
+    #     """Animals that can speak are correctly identified"""
+    #     self.assertEqual(2, 1)
+
+    def test_success(self):
+        """Animals that can speak are correctly identified"""
+        self.assertEqual(1, 1)
