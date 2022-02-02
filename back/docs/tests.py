ea7cedc7717121c914c5e6aa6b2de75b8639a457
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-02-02 12:13:16 +0100

    Failing test removed

--- back/docs/tests.py
+++ back/docs/tests.py
@@ -1,11 +1,7 @@
 from django.test import TestCase
 
 
-class AnimalTestCase(TestCase):
-
-    def test_fail(self):
-        """Animals that can speak are correctly identified"""
-        self.assertEqual(2, 1)
+class ExampleTest(TestCase):
 
     def test_success(self):
         """Animals that can speak are correctly identified"""
