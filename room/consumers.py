c8dc99a3f3415739251992e9693e9e1828bc1e89
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-02-25 00:54:59 +0100

    Consumer made sync

--- room/consumers.py
+++ room/consumers.py
@@ -1,30 +1,30 @@
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
 from account.models import Account
 from channels.auth import login, logout
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
         authenticated = isinstance(self.scope["user"], Account)
         if authenticated:
-            await self.accept()
+            self.accept()
             print('Connected', self.scope["user"])
         else:
-            await self.close()
+            self.close()
             print('Closed')
 
-    async def disconnect(self, close_code):
+    def disconnect(self, close_code):
         # await logout(self.scope)
         print('Disconnect', self.scope["user"])
 
     # Receive message from WebSocket
-    async def receive(self, text_data):
+    def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
         # Send message to room group
-        await self.channel_layer.group_send(
+        self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -33,10 +33,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     # Receive message from room group
-    async def chat_message(self, event):
+    def chat_message(self, event):
         message = event['message']
 
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({
+        self.send(text_data=json.dumps({
             'message': message
         }))
