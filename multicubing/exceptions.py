e460129c4539e89866551d43515f22e4d326d803
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-03-02 22:07:17 +0100

    Custom 404 error handler created

--- multicubing/exceptions.py
+++ multicubing/exceptions.py
@@ -1,5 +1,6 @@
 from rest_framework.views import exception_handler
 from rest_framework.exceptions import ValidationError
+from django.http.response import Http404
 from rest_framework import status
 from api.utils import ErrorResponse
 
@@ -11,5 +12,16 @@ def custom_exception_handler(exc, context):
             error='invalid-data',
             details=exc.args[0]
         )
-    print(exc)
+    if isinstance(exc, Http404):
+        return ErrorResponse(
+            status=status.HTTP_404_NOT_FOUND,
+            error='not-found',
+        )
     return exception_handler(exc, context)
+
+
+def custom_404_exception_handler(exc, context):
+    return ErrorResponse(
+        status=status.HTTP_400_BAD_REQUEST,
+        error='not-found',
+    )
