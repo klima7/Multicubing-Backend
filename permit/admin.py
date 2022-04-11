513cd9c11f0a3737dda1a005466eb4ef89537323
klima7 <ukasz.klimkiewicz@gmail.com>
2022-04-12 01:14:25 +0200

    Admin site added for Permit

--- permit/admin.py
+++ permit/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Permit
 
-admin.site.register(Permit)
+
+class PermitAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'room', 'account')
+
+
+admin.site.register(Permit, PermitAdmin)
--- permit/models.py
+++ permit/models.py
@@ -6,3 +6,6 @@ from account.models import Account
 class Permit(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.room.name} - {self.account.username}'
