c5a6e0e14b2b83177dd7be108f31214be420b244
klima7 <ukasz.klimkiewicz@gmail.com>
2022-04-15 02:12:19 +0200

    Admin view for messages improved

--- message/admin.py
+++ message/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .models import Message
 
 
-class PermitAdmin(admin.ModelAdmin):
-    list_display = ('content', 'sender')
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('content', 'sender', 'send_time')
 
 
-admin.site.register(Message, PermitAdmin)
+admin.site.register(Message, MessageAdmin)
