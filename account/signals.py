7ad65a74a1f2af06dfa1d61aa334cf1e4799624d
klima7 <ukasz.klimkiewicz@gmail.com>
2022-04-17 10:47:39 +0200

    users_changed signal splited

--- account/signals.py
+++ account/signals.py
@@ -9,35 +9,43 @@ from rest_framework.authtoken.models import Token
 from .models import Account
 
 
-connected_users_changed = django.dispatch.Signal()
+user_connected = django.dispatch.Signal()
+user_disconnected = django.dispatch.Signal()
 
 
 @receiver(presence_changed)
 def detect_connected_users_changed(sender, room, added, removed, **kwargs):
+    # check if correct room
     match = re.match(r'^account\.(.*)$', room.channel_name)
     if not match:
         return
 
+    # get associated account
     username = match.groups()[0]
     account = Account.objects.filter(username=username).first()
     if account is None:
         return
 
-    new_added = account if added else None
-    new_removed = account if removed else None
+    # send specific signals
+    if added:
+        user_connected.send(sender=sender, user=account)
+    if removed:
+        user_disconnected.send(sender=sender, user=account)
 
-    connected_users_changed.send(sender=sender, connected=new_added, disconnected=new_removed)
+
+@receiver(user_connected)
+def on_user_connected_update_last_seen(sender, user, **kwargs):
+    user.update_last_seen()
+    user.save()
+
+
+@receiver(user_disconnected)
+def on_user_disconnected_update_last_seen(sender, user, **kwargs):
+    user.update_last_seen()
+    user.save()
 
 
 @receiver(post_save, sender=Account)
 def on_account_creation_create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
-
-
-@receiver(connected_users_changed)
-def on_connected_users_changed_update_last_seen(sender, connected, disconnected, **kwargs):
-    account = connected if connected else disconnected
-    if account:
-        account.update_last_seen()
-        account.save()
