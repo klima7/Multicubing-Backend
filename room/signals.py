cfafbf40140a9d2599b00dc5d7ff70e111012980
klima7 <ukasz.klimkiewicz@gmail.com>
2022-04-17 11:38:13 +0200

    Signals defs moved to separate module

--- room/signals.py
+++ room/signals.py
@@ -1,41 +1,13 @@
-import re
-
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
-from presence.signals import presence_changed
-import django
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
-from multicubing.signals import save_done
 from account.serializers import AccountSerializer
+from multicubing.signals import save_done
 from .models import Room
 from .serializers import RoomsReadSerializer
-
-
-# room participants signals
-room_connected = django.dispatch.Signal()
-room_disconnected = django.dispatch.Signal()
-
-
-@receiver(presence_changed)
-def detect_room_connections(sender, room, added, removed, **kwargs):
-    # check if proper room
-    match = re.match(r'^rooms\.(.*)$', room.channel_name)
-    if not match:
-        return
-
-    # get room
-    room_slug = match.groups()[0]
-    real_room = Room.objects.filter(slug=room_slug).first()
-    if not real_room:
-        return
-
-    # send specific signal
-    if added:
-        room_connected.send(sender=sender, room=real_room, user=added.user)
-    if removed:
-        room_disconnected.send(sender=sender, room=real_room, user=removed.user)
+from .signals_def import *
 
 
 @receiver((room_connected, room_disconnected))
@@ -44,14 +16,14 @@ def on_connection_send_room_notification(sender, room, user, **kwargs):
 
 
 @receiver(room_connected)
-def on_room_participants_changed_send_users_notifications(sender, room, user, **kwargs):
+def on_room_connected_send_users_notifications(sender, room, user, **kwargs):
     channel_name = f'rooms.{room.slug}'
     serialized_user = AccountSerializer(user).data
     async_to_sync(get_channel_layer().group_send)(channel_name, {'type': 'users.update', 'user': serialized_user})
 
 
 @receiver(room_disconnected)
-def on_room_participants_changed_send_users_notifications(sender, room, user, **kwargs):
+def on_room_disconnected_send_users_notifications(sender, room, user, **kwargs):
     channel_name = f'rooms.{room.slug}'
     async_to_sync(get_channel_layer().group_send)(channel_name, {'type': 'users.delete', 'username': user.username})
 
---
+++ room/signals_def.py
@@ -0,0 +1,43 @@
+import re
+
+import django
+from django.dispatch import receiver
+
+from presence.signals import presence_changed
+from .models import Room
+
+
+__all__ = [
+    'room_connected',
+    'room_disconnected',
+    'room_first_connected',
+    'room_last_disconnected'
+]
+
+
+room_connected = django.dispatch.Signal()
+room_disconnected = django.dispatch.Signal()
+
+
+room_first_connected = django.dispatch.Signal()
+room_last_disconnected = django.dispatch.Signal()
+
+
+@receiver(presence_changed)
+def detect_room_connections(sender, room, added, removed, **kwargs):
+    # check if proper room
+    match = re.match(r'^rooms\.(.*)$', room.channel_name)
+    if not match:
+        return
+
+    # get room
+    room_slug = match.groups()[0]
+    real_room = Room.objects.filter(slug=room_slug).first()
+    if not real_room:
+        return
+
+    # send specific signal
+    if added:
+        room_connected.send(sender=sender, room=real_room, user=added.user)
+    if removed:
+        room_disconnected.send(sender=sender, room=real_room, user=removed.user)
