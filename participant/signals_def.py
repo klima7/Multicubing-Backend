dac2571e32c6f6da3405858b2a83b79520c41124
klima7 <ukasz.klimkiewicz@gmail.com>
2022-04-18 14:49:01 +0200

    room_connected_with_participant signal created

--- participant/signals_def.py
+++ participant/signals_def.py
@@ -7,10 +7,12 @@ from .models import Participant
 
 __all__ = [
     'room_connected_without_participant',
+    'room_connected_with_participant',
 ]
 
 
 room_connected_without_participant = django.dispatch.Signal()
+room_connected_with_participant = django.dispatch.Signal()
 
 
 @receiver(room_connected)
@@ -18,3 +20,5 @@ def detect_room_connected_without_participant(sender, room, user, **kwargs):
     participant = Participant.objects.filter(room=room, user=user).first()
     if participant is None:
         room_connected_without_participant.send(sender=sender, room=room, user=user)
+    else:
+        room_connected_with_participant.send(sender=sender, room=room, participant=participant)
