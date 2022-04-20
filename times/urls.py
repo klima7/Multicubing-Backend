604206ce9d2bf4711817510614bb70eca9e492b9
klima7 <ukasz.klimkiewicz@gmail.com>
2022-04-21 00:16:36 +0200

    NestedTimesView endpoint removed

--- times/urls.py
+++ times/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .views import TimesView, TurnsView, TurnView, NestedTimesView, NestedTimeView
+from .views import TimesView, TurnsView, TurnView, NestedTimeView
 
 urlpatterns = [
-    path('rooms/<room_slug>/times/', TimesView.as_view()),
     path('rooms/<room_slug>/turns/', TurnsView.as_view()),
     path('rooms/<room_slug>/turns/<int:turn_number>/', TurnView.as_view()),
-    path('rooms/<room_slug>/turns/<int:turn_number>/times/', NestedTimesView.as_view()),
+    path('rooms/<room_slug>/times/', TimesView.as_view()),
     path('rooms/<room_slug>/turns/<int:turn_number>/times/<username>/', NestedTimeView.as_view()),
 ]
