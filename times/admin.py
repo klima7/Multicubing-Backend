4ba21a728427b4ca1748d6e0f7e74c7d2b89f919
klima7 <ukasz.klimkiewicz@gmail.com>
2022-04-21 00:35:52 +0200

    Inlined Time added to Turn admin interface

--- times/admin.py
+++ times/admin.py
@@ -3,15 +3,24 @@ from django.contrib import admin
 from .models import Turn, Time
 
 
-class TurnAdmin(admin.ModelAdmin):
-    list_display = ('number', 'room', 'scramble')
-
-
-admin.site.register(Turn, TurnAdmin)
-
-
 class TimeAdmin(admin.ModelAdmin):
     list_display = ('time', 'user', 'flag', 'turn')
 
 
 admin.site.register(Time, TimeAdmin)
+
+
+class TimeInlineAdmin(admin.TabularInline):
+    model = Time
+    extra = 0
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+class TurnAdmin(admin.ModelAdmin):
+    list_display = ('number', 'room', 'scramble')
+    inlines = [TimeInlineAdmin]
+
+
+admin.site.register(Turn, TurnAdmin)
